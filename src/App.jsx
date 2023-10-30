705206dfb42fa07d2d516c5bc725fd518fe14815
Jhonny Callisaya <jhonnyeddy.22@gmail.com>
2023-10-30 01:31:29 -0400

    add icon and title and hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
 import { useState, Fragment } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './App.css'
 
 // import components
-import { Home } from './pages/Home'
-import { InventoryMaterial } from './pages/InventoryMaterial';
-import { Labour } from './pages/Labour';
-import { IndirectCosts } from './pages/IndirectCosts'
-import { Users } from './pages/Users'
-import { NotFound } from './pages/NotFound'
-import { User } from './pages/User'
-import { Login } from './pages/Login'
-import MiniDrawer from './components/MiniDrawer'
+import MiniDrawer from './components/MiniDrawer';
 
 function App() {
-  const [auth, setAuth] = useState(true)
-
   return (
-    <BrowserRouter>
+    <HashRouter>
       <MiniDrawer/>
-      
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
-export default App
+export default App;
